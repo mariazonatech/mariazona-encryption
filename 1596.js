6f719cb8b11696e0f910b482b6ce2bb9ad4c38fb
nidexinggg <148669926+nidexinggg@users.noreply.github.com>
2023-12-13 23:49:02 +0700

    Update 1596.js

--- 1596.js
+++ 1596.js
@@ -1,7 +1,49 @@
-var isUnlocked = false;function login() {
+const logInBox = document.getElementById("loginCode");
+      document.addEventListener ("keydown", e => {
+        if (e.ctrlKey && e.key.toLowerCase() === "k" || e.key.toLowerCase() === "k" && e.metaKey) {
+          e.preventDefault();
+          logInBox.focus();
+        }
+      });
+      document.addEventListener('keydown', function(event) {
+        if (event.keyCode === 13) {
+          document.getElementById('loginBtn').click();
+        }
+      });
+      const clear = document.getElementById("clearBtn");
+      document.addEventListener ("keydown", e => {
+        if (e.ctrlKey && e.key.toLowerCase() === "x" || e.key.toLowerCase() === "x" && e.metaKey) {
+          e.preventDefault();
+          clear.click();
+        }
+      });
+      // const encrypt = document.getElementById("encryptBtn");
+      // document.addEventListener ("keydown", e => {
+      //   if (e.ctrlKey && e.key.toLowerCase() === "e" || e.key.toLowerCase() === "e" && e.metaKey) {
+      //     e.preventDefault();
+      //     encrypt.click();
+      //   }
+      // });
+      // const decrypt = document.getElementById("decryptBtn");
+      // document.addEventListener ("keydown", e => {
+      //   if (e.ctrlKey && e.key.toLowerCase() === "d" || e.key.toLowerCase() === "d" && e.metaKey) {
+      //     e.preventDefault();
+      //     decrypt.click();
+      //   }
+      // });
+      const textBox = document.getElementById("input");
+      document.addEventListener ("keydown", e => {
+        if (e.key.toLowerCase() === "/") {
+          e.preventDefault();
+          textBox.focus();
+        }
+      });
+
+      var isUnlocked = false;
+      function login() {
         var enteredCode = document.getElementById("loginCode").value;
         if (enteredCode !== token) {
-          return;
+          return swal("Authentication Failed", "😂😂😂😂😂");
         }
         document.getElementById("loginCode").value = "";
         document.getElementById("loginSection").style.display = "none";
@@ -16,50 +58,26 @@ var isUnlocked = false;function login() {
         if (!isUnlocked) {
           isUnlocked = true;
       }
-      var inputElement = document.getElementById("input");
-      var outputElement = document.getElementById("output");
-      var inputText = inputElement.value;
-      var encryptionKey = "your-random-encryption-key";
-      var encryptedText = CryptoJS.AES.encrypt(inputText, encryptionKey).toString();
-      outputElement.value = encryptedText;
+        var inputElement = document.getElementById("input");
+        var outputElement = document.getElementById("output");
+        var inputText = inputElement.value;
+        var encryptionKey = "your-random-encryption-key";
+        var encryptedText = CryptoJS.AES.encrypt(inputText, encryptionKey).toString();
+        outputElement.value = encryptedText;
       }
       function decrypt() {
         if (!isUnlocked) {
           isUnlocked = true;
         }
-      var inputElement = document.getElementById("input");
-      var outputElement = document.getElementById("output");
-      var encryptedText = inputElement.value;
-      var encryptionKey = "your-random-encryption-key";var decryptedText = CryptoJS.AES.decrypt(encryptedText, encryptionKey).toString(CryptoJS.enc.Utf8);outputElement.value = decryptedText;
-          }
+        var inputElement = document.getElementById("input");
+        var outputElement = document.getElementById("output");
+        var encryptedText = inputElement.value;
+        var encryptionKey = "your-random-encryption-key";
+        var decryptedText = CryptoJS.AES.decrypt(encryptedText, encryptionKey).toString(CryptoJS.enc.Utf8);
+        outputElement.value = decryptedText;
+      }
       function clearInput() {
         document.getElementById("input").value = "";
       }
-      function toggleDarkMode() {
-      var bodyElement = document.body;
-      bodyElement.classList.toggle("dark-mode");
 
-      var isDarkMode = bodyElement.classList.contains("dark-mode");
-
-      localStorage.setItem('darkMode', isDarkMode);
-
-      var toggleSwitch = document.querySelector(".toggle-switch");
-      var toggleTextLight = document.querySelector(".toggle-text.light");
-      var toggleTextDark = document.querySelector(".toggle-text.dark");
-
-      toggleSwitch.classList.toggle("on");
-      toggleTextLight.classList.toggle("hidden");
-      toggleTextDark.classList.toggle("hidden");
-  }
-
-    // Check user preference on page load
-    document.addEventListener('DOMContentLoaded', (event) => {
-        var bodyElement = document.body;
-        var isDarkMode = localStorage.getItem('darkMode') === 'true';
-
-        if (isDarkMode) {
-            bodyElement.classList.add('dark-mode');
-        }
-    });
-
-      var token = "darachan007";
+      var token ="johnyjohnyyespapa";
